5274ba23d0455afe582eb6ff6bd27a8f9d6e6368
dinn <dinn.ding@vechain.com>
2019-04-11 12:23:32 +0800

    dev

--- src/sequelize-models/index.ts
+++ src/sequelize-models/index.ts
@@ -1,14 +1,25 @@
 import { Sequelize } from 'sequelize-typescript';
 import Record from './record.models';
 import Schedule from './schedule.model';
-
-const sequelize = new Sequelize({
-    database: 'Faucet',
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '123456qwe',
-    models: [Record, Schedule]
-});
+let sequelize: Sequelize;
+if (!process.env.NODE_ENV || process.env.NODE_ENV == "dev") {
+    sequelize = new Sequelize({
+        database: 'Faucet',
+        dialect: 'mysql',
+        host: 'localhost',
+        username: 'root',
+        password: '123456qwe',
+        models: [Record, Schedule]
+    });
+} else {
+    sequelize = new Sequelize({
+        database: 'Faucet',
+        dialect: 'mysql',
+        host: 'localhost',
+        username: 'root',
+        password: 'vechain@faucet!',
+        models: [Record, Schedule]
+    });
+}
 
 export { sequelize }
